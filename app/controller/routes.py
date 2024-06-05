013b2d852750d4d16aa7981508c2ac164c37a877
aasjunior <adeldivo@hotmail.com>
2024-06-05 10:29:12 -0300

    /test-knn

--- app/controller/routes.py
+++ app/controller/routes.py
@@ -1,12 +1,11 @@
 from fastapi import APIRouter, HTTPException, status, UploadFile, File
 from model.DataScheme import DataScheme
 from service.knn import apply_knn
-from model.DataModel import DataModel
 import pandas as pd
 
 router = APIRouter()
 
-@router.post("/knn")
+@router.post('/knn')
 async def knn(data: DataScheme, file: UploadFile = File(...)):
     try:
         contents = await file.read()
@@ -17,16 +16,41 @@ async def knn(data: DataScheme, file: UploadFile = File(...)):
         accuracy = apply_knn(X, y)
 
         info = {
-            "quantidade de exemplos": len(df),
-            "quantidade de classes": df[data.classHeader].nunique(),
-            "quantidade de atributos": len(data.attributeHeaders),
-            "taxa de acerto": "%.2f%%" % accuracy,
+            'quantidade de exemplos': len(df),
+            'quantidade de classes': df[data.classHeader].nunique(),
+            'quantidade de atributos': len(data.attributeHeaders),
+            'taxa de acerto': '%.2f%%' % accuracy,
         }
 
-        return {"result": info}
+        return {'result': info}
 
     except Exception as e:
         return HTTPException(status_code=400, detail=str(e))
+
+@router.get('/test-knn')
+async def test_knn():
+    attribute_headers = ['sepal_length','sepal_width','petal_length','petal_width']
+    class_header = 'class'
+    try:
+
+        df = pd.read_csv('db/iris.csv')
+        X = df[list(attribute_headers)]
+        y = df[class_header]
+
+        accuracy = apply_knn(X, y)
+
+        info = {
+            'quantidade de exemplos': len(df),
+            'quantidade de classes': df[class_header].nunique(),
+            'quantidade de atributos': len(attribute_headers),
+            'taxa de acerto': accuracy,
+        }
+
+        return {'result': info}
+
+    except Exception as e:
+        return HTTPException(status_code=400, detail=str(e))
+
 
 @router.get("/genetic-algorithm")
 async def genetic_algorithm():
--- app/service/knn.py
+++ app/service/knn.py
@@ -9,6 +9,7 @@ import pandas as pd
 def apply_knn(X: DataFrame, y: DataFrame, test_size=0.3, train_size=0.7, n_neighbors=3):
     try:
         if not is_normalized(X):
+            print('\nis not normalized\n')
             X, y = normalize_data(X, y)
 
         X_train, X_test, Y_train, Y_test = train_test_split(X, y, test_size=test_size, train_size=train_size)
@@ -18,7 +19,7 @@ def apply_knn(X: DataFrame, y: DataFrame, test_size=0.3, train_size=0.7, n_neigh
 
         predictions = knn.predict(X_test)
         accuracy = accuracy_score(Y_test, predictions) * 100
-
+    
         return  "%.2f%%" % accuracy
     
     except Exception as e:
@@ -30,13 +31,20 @@ def normalize_data(X: DataFrame, y: DataFrame):
     
     for col in X.columns:
         if X[col].dtype == 'object':
+            print(f'\nAplicando encoder:\n{X.columns}\n')
             X[col] = encoder.fit_transform(X[col])
+            print(f'\n{X[col]}\n')
     
+    print(f'\nX:\n{X}\n')
     X = pd.DataFrame(scaler.fit_transform(X), columns=X.columns)
+    print(f'\nX transform:\n{X}\n')
 
+    print(f'\ny:\n{y}\n')
     if y.dtype == 'object':
         y = encoder.fit_transform(y)
+        print(f'\ny transform:\n{y}\n')
         y = pd.Series(y.flatten())
+        print(f'\ny flatten:\n{y}\n')
     
     return X, y
 
