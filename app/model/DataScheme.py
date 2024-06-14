90a6effe23b1309d5de132e1ef9821a082fd8b86
Adeldivo Junior <61213599+aasjunior@users.noreply.github.com>
2024-06-14 20:56:51 -0300

    Update DataScheme.py
    from_string

--- app/model/DataScheme.py
+++ app/model/DataScheme.py
@@ -1,7 +1,12 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 from typing import Set
+import json
 
 class DataScheme(BaseModel):
-    src: str
     attributeHeaders: Set[str]
     classHeader: str
+
+    @classmethod
+    def from_string(cls, data_string: str):
+        data_dict = json.loads(data_string)
+        return cls(**data_dict)
