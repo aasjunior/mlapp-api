c5acf95b6c5149742f63de51bfe2ab30ab047cf7
Adeldivo Junior <61213599+aasjunior@users.noreply.github.com>
2024-06-14 20:58:41 -0300

    fix
    POST /knn e POST /decision-tree

--- app/controller/routes.py
+++ app/controller/routes.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, HTTPException, status, UploadFile, File
+from fastapi import APIRouter, HTTPException, status, UploadFile, File, Body
 from model.DataScheme import DataScheme
 from service.knn import apply_knn
 from service.decision_tree import apply_decision_tree
@@ -6,27 +6,31 @@ from service.genetic_algorithm import apply_genetic_algorithm
 from service.utils import generate_log
 import pandas as pd
 import traceback
+import io
 
 router = APIRouter()
 
 data_src = "assets/db/iris.csv"
 
 @router.post('/knn')
-async def knn(data: DataScheme, file: UploadFile = File(...)):
+async def knn(data_string: str = Body(..., alias="data"), file: UploadFile = File(...)):
     try:
+        data = DataScheme.from_string(data_string)
+    
         contents = await file.read()
-        df = pd.read_csv(contents)
+        df = pd.read_csv(io.BytesIO(contents))
         X = df[list(data.attributeHeaders)]
         y = df[data.classHeader]
 
         accuracy = apply_knn(X, y)
 
+        print(f'\naccuracy: {accuracy}\n')
 
         return {
             'number_of_examples': len(df),
             'number_of_classes': df[data.classHeader].nunique(),
             'number_of_attributes': len(data.attributeHeaders),
-            'accuracy': '%.2f%%' % accuracy
+            'accuracy': accuracy
         }
 
     except Exception as e:
@@ -59,10 +63,12 @@ async def test_knn():
 
 
 @router.post('/decision-tree')
-async def decision_tree(data: DataScheme, file: UploadFile = File(...)):
+async def decision_tree(data_string: str = Body(..., alias="data"), file: UploadFile = File(...)):
     try:
+        data = DataScheme.from_string(data_string)
+    
         contents = await file.read()
-        df = pd.read_csv(contents)
+        df = pd.read_csv(io.BytesIO(contents))
         X = df[list(data.attributeHeaders)]
         y = df[data.classHeader]
 
